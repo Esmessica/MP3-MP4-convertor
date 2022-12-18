51847d3b07bda980e006d4ee4ac843bbfc09d96a
Anna Lorencová <a.lorencov@seznam.cz>
2022-12-18 23:42:52 +0100

    File hierarchy change, methods change

--- pygubu_window.py
+++ pygubu_window.py
@@ -6,7 +6,7 @@ PROJECT_PATH = pathlib.Path(__file__).parent
 PROJECT_UI = PROJECT_PATH / "window_downloader.ui"
 
 
-class WindowDownloaderApp:
+class Converthot:
     def __init__(self, master=None):
         self.builder = builder = pygubu.Builder()
         builder.add_resource_path(PROJECT_PATH)
@@ -14,7 +14,6 @@ class WindowDownloaderApp:
         # Main widget
         self.mainwindow = builder.get_object("toplevel1", master)
         builder.connect_callbacks(self)
-
         self.entry_user_url = builder.get_object("entry_user_url")
 
     def run(self):
@@ -22,5 +21,9 @@ class WindowDownloaderApp:
 
     def on_download_button_click(self):
         user_answer = self.entry_user_url.get()
-        if user_answer == "url":
-            print("good")
+
+
+
+if __name__ == "__main__":
+    app = Converthot()
+    app.run()
