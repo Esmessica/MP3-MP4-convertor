ef06fbd72cf83787c3d9b0f9e5b210dd51c1fbb2
Anna Lorencová <a.lorencov@seznam.cz>
2022-12-18 23:21:55 +0100

    Pygubu window design

---
+++ pygubu_window.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+import pathlib
+import pygubu
+
+PROJECT_PATH = pathlib.Path(__file__).parent
+PROJECT_UI = PROJECT_PATH / "window_downloader.ui"
+
+
+class WindowDownloaderApp:
+    def __init__(self, master=None):
+        self.builder = builder = pygubu.Builder()
+        builder.add_resource_path(PROJECT_PATH)
+        builder.add_from_file(PROJECT_UI)
+        # Main widget
+        self.mainwindow = builder.get_object("toplevel1", master)
+        builder.connect_callbacks(self)
+
+        self.entry_user_url = builder.get_object("entry_user_url")
+
+    def run(self):
+        self.mainwindow.mainloop()
+
+    def on_download_button_click(self):
+        user_answer = self.entry_user_url.get()
+        if user_answer == "url":
+            print("good")
