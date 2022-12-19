f7403ea294ebe276737a685e2c4143a28b27e6b8
Anna Lorencová <a.lorencov@seznam.cz>
2022-12-19 23:55:38 +0100

    Adding commentary in main.py

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
 from pygubu_window import Converthot
 
 pyg_window = Converthot()
+# make functioning button in GUI
+
+# make it get url from enter bar
+
+# make it choose to do mp3 file or mp4 file
